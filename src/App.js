10493f7f936843f5368a3f33ed1979a847305c25
VaNomad <sjur.hassel@gmail.com>
2023-05-01 19:08:28 +0100

    learning components and props

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import './App.css';
 
-function App() {
+const App = () => {
+
+  const Person = (props) => {
+    return (
+      <>
+        <h1>Name: {props.name}</h1>
+        <h2>Last Name: {props.lastName}</h2>
+        <h2>Age: {props.age}</h2>
+      </>
+    )
+  }
+
   return (
     <div className="App">
-      
+      <Person name={"Jane"} lastName={"Doe"} age={"38"} />
+      <Person name={"Sjur"} lastName={"Hassel"} age={"48"} />
+      <Person />
+      <Person />
     </div>
   );
 }
